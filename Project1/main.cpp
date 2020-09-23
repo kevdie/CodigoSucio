6b29a0b4eaaec01f75602c0b3b6ca639644e43c7
Carolina <carito15villalobos@gmail.com>
2020-09-23 17:51:51 -0400

    refactor code

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,44 +1,49 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-bool mostrar(int a[])
+
+const int sizeVector = 10; //variables globales y constantes
+const string file = "numeros.txt";
+int vector[sizeVector]; //nombres de variables mas entendibles
+//nombres de variables y funciones en ingles
+void verifyMajor(int index1, int index2) ///metodo nuevo
 {
-	for (int i = 0; i < 10; i++)
-	{
-		cout << a[i] << endl;
-	}
-	return true;
+	if (vector[index1] > vector[index2])
+		swap(vector[index1], vector[index2]);
 }
+
+void sort()
+{
+	for (int i = 0; i < sizeVector - 1; i++)
+		for (int j = i + 1; j < sizeVector; j++)
+			verifyMajor(i, j);
+}
+
+void readArchive()
+{
+	int cont = 0; ///eliminar variables no usadas
+	ifstream file(file);
+	while (!file.eof())
+	{
+		file >> vector[cont];
+		cont++;
+	}
+	file.close();
+}
+
+void showVector() ///cambiar de funcion a metodo ya que no sirve de nada un boolean
+{
+	for (int i = 0; i < sizeVector; i++)
+		cout << vector[i] << endl; ///reduccion de lineas para un for
+}
+
+//eliminar if no necesario
+///eliminar texto innecesario
+
 int main()
 {
-	int a[10];
-	int b, c, d = 0;
-	ifstream e("numeros.txt");
-	while (!e.eof())
-	{
-		e >> a[d];
-		d++;
-	}
-	e.close();
-	for (int i = 0; i < 10 - 1; i++)
-	{
-		for (int j = i + 1; j < 10; j++)
-		{
-			if (a[i] > a[j])
-			{
-				swap(a[i], a[j]);
-			}
-				
-		}
-	}		
-	for (int i = 0; i < 10; i++)
-	{
-		cout << a[i] << endl;
-	}
-	if (mostrar(a))
-	{
-		cout << "vector mostrado";
-	}
-		
+	readArchive();
+	sort();
+	showVector();
 	return 0;
 }
